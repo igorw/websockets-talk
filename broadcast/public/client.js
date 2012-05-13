1fdc1770ad955af0619585c716798e36c08c5e7f
Igor Wiedler <igor@wiedler.ch>
2012-05-13 22:15:52 +0200

    Move client code to client.js, make it more robust

---
+++ broadcast/public/client.js
@@ -0,0 +1,22 @@
+var sock = new SockJS('http://localhost:8080/broadcast');
+sock.onmessage = function (event) {
+    var parts, type, data;
+
+    // message format: type [data]
+    // type: \w+
+    // data: JSON
+
+    parts = event.data.match(/(\w+)( (.+)|$)/);
+    if (!parts) {
+        throw "Invalid message provided: "+event.data;
+    }
+
+    type = parts[1];
+    data = parts[3] ? JSON.parse(parts[3]) : null;
+
+    switch (type) {
+        case 'debug':
+            console.log(data);
+            break;
+    }
+};
